ce999492d24698b7625cb0d6cdb62bceff9de8c7
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2020-03-24 16:37:47 -0300

    fim da aula 4 parte 2

---
+++ Assets/Scritpt/VolumeControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+
+public class VolumeControl : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer audioMixer;
+    [SerializeField]
+    private string mixerParam;
+    [SerializeField]
+    private OnLoadSuccess OnLoadSuccess;
+    private void Start()
+    {
+        if(PlayerPrefs.HasKey(mixerParam))
+        {
+            PlayerPrefs.GetFloat(mixerParam);
+            float vol = PlayerPrefs.GetFloat(mixerParam);
+            audioMixer.SetFloat(mixerParam, vol);
+            OnLoadSuccess.Invoke(vol);
+        }
+    }
+    public void ChangeVolume(float volume)
+    {
+        audioMixer.SetFloat(mixerParam, volume);
+        PlayerPrefs.SetFloat(mixerParam, volume);
+    }
+}
+[System.Serializable]
+public class OnLoadSuccess : UnityEvent<float>
+{ }
+
